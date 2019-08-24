dddfbb8965a8fe10a58c49eb11f339d367b65e17
Isa Muriel <isamuriel@MacBook-Air-de-Isa.local>
2019-08-24 12:19:12 -0500

    DenseNet201

--- denseNet.py
+++ denseNet.py
@@ -1,3 +1,15 @@
-from keras.applications.densenet import DenseNet201
+from keras.applications.densenet import DenseNet201, preprocess_input, decode_predictions
+from keras.preprocessing import image
+import numpy as np
 
-DenseNet201(include_top=False, weights='imagenet', input_tensor=None, input_shape=(200,200,3), pooling='avg', classes=2)
+model = DenseNet201(weights="imagenet")
+
+img_path = ''
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict(x)
+
+print('Predicted:', decode_predictions(preds, top=3)[0])
